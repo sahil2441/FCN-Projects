f51fa9b0eab69f90828d2045558b68da7d8a6e69
Sahil Jain <sahil.2441@gmail.com>
2016-02-13 20:33:04 -0500

    Few more changes

--- fcn_hw1/src/fcn_hw/DNSResolver.java
+++ fcn_hw1/src/fcn_hw/DNSResolver.java
@@ -2,6 +2,7 @@ package fcn_hw;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,22 +20,27 @@ public class DNSResolver {
 
 	public static void main(String[] args) {
 		String website = "www.google.com.";
-		String root = "192.5.5.241";
+		List<String> rootServers = getRootServerList();
+		boolean success = false;
 
-		while (true) {
-			try {
-				if (resolveAddress(website, root)) {
-					System.out.println("DNS Resolved for :" + website);
-					printIPAddress();
-					break;
-				} else
-					System.out.println("Failed to Resolved DNS for :" + website);
+		for (int i = 0; i < rootServers.size(); i++) {
+			if (success)
+				break;
+			String root = rootServers.get(i);
+			while (true) {
+				try {
+					if (resolveAddress(website, root)) {
+						System.out.println("DNS Resolved for :" + website);
+						printIPAddress();
+						success = true;
+						break;
+					} else
+						System.out.println("Failed to Resolved DNS for :" + website);
 
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				} catch (IOException e) {
+					// Do nothing
+				}
 			}
-
 		}
 
 	}
@@ -98,4 +104,12 @@ public class DNSResolver {
 
 		return ipAddresses;
 	}
+
+	private static List<String> getRootServerList() {
+		String[] array = { "198.41.0.4", "192.228.79.201", "192.33.4.12", "199.7.91.13", "192.203.230.10",
+				"192.5.5.241", "192.112.36.4", "198.97.190.53", "192.36.148.17", "192.58.128.30", "193.0.14.129",
+				"199.7.83.42", "202.12.27.33", };
+		return Arrays.asList(array);
+	}
+
 }
