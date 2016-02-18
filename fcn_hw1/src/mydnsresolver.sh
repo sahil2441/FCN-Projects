82f0e251c2a9706ec47c40f6d46432df5e0e7315
Sahil Jain <sahil.2441@gmail.com>
2016-02-17 19:02:25 -0500

    Restructured

---
+++ fcn_hw1/src/mydnsresolver.sh
@@ -0,0 +1,6 @@
+
+#Compile
+javac -cp org.xbill.dns_2.1.7.jar DNSResolver.java
+
+# Run the program by giving the jar in classpath
+java -jar ../org.xbill.dns_2.1.7.jar fcn_hw.DNSResolver www.facebook.com 
